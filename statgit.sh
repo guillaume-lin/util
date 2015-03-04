31f634f241b439d00d5129bad7a773e3586d802b
Jingxian Lin <jingxian.lin@tpv-tech.com>
2015-03-04 19:01:59 +0800

    commit work

--- statgit.sh
+++ statgit.sh
@@ -1,54 +1,66 @@
 #!/bin/bash
+#
+#
+#  stat a git repository, output code changed by each author
+#  this tool use the following git command:
+#  git log --after=2015-2-1T0:0:0 --before=2015-3-2T0:0:0  --shortstat --pretty=format:"%an;%ai"
+#
+#
+#  Usage: statgit.sh <start date> <end date> <git path> <out dir>
+#
+#  this shell script will output a .csv file with following data:
+#  author, date, insertions, deletions
+#
 
-
-
+if [ $# -lt 4 ] ; 
+then
+	echo "Usage: statgit.sh <start date> <end date> <git repository path> <out dir>";
+	exit -1;
+fi
+SDATE=$1T0:0:0
+EDATE=$2T0:0:0
+GPATH=$3
+OUT=$4
+TMP=`mktemp /tmp/GITLOG-XXX.log`
+OUT_FILE=$3/gitstat.csv
+git log --after=$SDATE --before=$EDATE --shortstat --pretty=format:"%an;%ai"  $GPATH > $TMP
+echo "author, date, insertions, deletions" >> $OUT_FILE
+author=""
+day=""
 function count() {
     local insert=0
     local delete=0
-    while read line ;do
-        current=`echo $line| awk -F',' '{printf $2}' | awk '{printf $1}'`
+    while read line ; do
+	echo "got line: [$line]"
+	if [[ -z $line ]]; then
+		echo "empty line"
+		continue ;
+	fi
+
+	# get the author and date
+        author=`echo $line| awk -F';' '{printf $1}' `
+	day=`echo $line | awk -F';' '{printf $2}' `
+	if [[ $line!=$author ]] && [[ $line!=$day ]] ; then
+		echo "got  author $author and day $day"
+		read line;
+	else
+		echo "not got author and day"
+		continue;
+	fi
+        
+	current=`echo $line | awk -F',' '{printf $2}' | awk '{printf $1}'`
         if [[ -n $current ]]; then 
-            insert=`expr $insert + $current`
+            insert=`expr $current`
         fi
         current=`echo $line | sed -n 's/.*, //p' | awk '{printf $1}'`
         if [[ -n $current ]]; then
-            delete=`expr $delete + $current`
+            delete=`expr $current`
         fi
-    done < /tmp/count
-    echo "$insert insertions, $delete deletions"
+	echo $line
+    	echo "$author, $day, $insert insertions, $delete deletions"
+	echo "$author, $day, $insert, $delete">>$OUT_FILE
+
+    done < $TMP
 }
 
-function countAll() {
-    git log --author=msdx --shortstat --pretty=format:"" | sed /^$/d >/tmp/count
-    count;
-    rm /tmp/count
-}
-
-function countToday() {
-    local current=`date +%s`;
-    local begin=`date +%Y-%m-%d |xargs date +%s -d`;
-    local minutes=$(($current - $begin));
-
-    git log --author=msdx --since="$minutes seconds ago" --shortstat --pretty=format:"" | sed /^$/d >/tmp/count
-    count;
-    rm /tmp/count
-
-}
-
-function countOneDay() {
-    git log --author=$1 --since="1 days ago" --shortstat --pretty=format:"" | sed /^$/d >/tmp/count
-    count;
-    rm /tmp/count
-
-}
-
-if [[ ! -n $1 ]] || [[ $1 = "all" ]] ; then 
-    countAll;
-elif [[ $1 = "oneday" ]]; then
-    countOneDay $2;
-elif [[ $1 = "today" ]]; then
-    countToday;
-else
-    echo "args: all | oneday | today";
-fi
-
+count;
--- statsvn.sh
+++ statsvn.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #
 # svn log -v --xml  -r {2015-1-1}:{2015-3-1}
-# statsvn.sh <author> <start date> <end date> <path> <out dir>
+# statsvn.sh <start date> <end date> <path> <out dir>
 #
+
+if [ $# -lt 4 ] ;
+then
+	echo "Usage: statsvn.sh <start date> <end date> <svn path> <out dir>";
+	exit -1;
+fi
+
 AUTHOR=$1
 SDATE=$2
 EDATE=$3
@@ -11,4 +18,4 @@ OUT=$5
 LOG=`mktemp svnlogXXX.xml`
 echo $AUTHOR $SDATE $EDATE $SVN_PATH $OUT
 svn log $SVN_PATH -v --xml -r{$SDATE}:{$EDATE} > $LOG
-java -jar /home/jingxian/util/statsvn.jar   -include "**/*.java;**/*.xml" -output-dir $OUT $LOG $SVN_PATH
+java -jar /home/jingxian/util/statsvn.jar   -include "**/com/tpv/**/*.java;**/*.xml" -exclude "bin/**;doc/**" -output-dir $OUT $LOG $SVN_PATH
