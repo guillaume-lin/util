8eac86862f8094abbb1365abdb472e588cb41ce1
ljx <root@be-test.hengan.shop>
2023-11-10 14:57:07 +0800

    auth

--- elixir/hello/mix.exs
+++ elixir/hello/mix.exs
@@ -32,6 +32,7 @@ defmodule Hello.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:luerl, "~>1.0"},
       {:phoenix, "~> 1.7.9"},
       {:phoenix_ecto, "~> 4.4"},
